478348fee3f7ca4cb2999dcb13298ee8374731b0
okafor stella <stellachommy01@gmail.com>
2023-12-03 14:44:19 -1100

    started apartment page

--- js/main.js
+++ js/main.js
@@ -9,3 +9,24 @@ const menuBtn = document.getElementById("show"),
   closeBtn.addEventListener("click", () => {
     mobileNav.style.right = "-100%";
   })
+
+// owl carousel2
+
+  $(document).ready(function(){
+    $('.carousel2').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:true,
+      responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        1024:{
+            items:3
+        }
+      }
+    })
+  });
